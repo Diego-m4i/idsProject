e6f4884a4e5552dc9a5224ed3c7a5b280278ce74
Diego Marinangeli <marinangelidiego@gmail.com>
2024-05-14 16:56:26 +0200

    Commit

---
+++ idsProject/src/main/java/com/cs/idsProject/IdsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cs.idsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdsProjectApplication.class, args);
+	}
+
+}
---
+++ idsProject/src/test/java/com/cs/idsProject/IdsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.idsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
