be10f21ba7b982edd8a5bb8ed29cda6f6283652f
Diego Marinangeli <marinangelidiego@gmail.com>
2024-06-04 12:13:11 +0200

    UtenteService.java

---
+++ idsProject/src/main/java/com/cs/idsProject/service/UtenteService.java
@@ -0,0 +1,33 @@
+package com.cs.idsProject.service;
+
+
+
+import com.cs.idsProject.entity.Utente;
+import com.cs.idsProject.repository.UtenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UtenteService {
+    @Autowired
+    private UtenteRepository utenteRepository;
+
+    public List<Utente> getAllUsers() {
+        return utenteRepository.findAll();
+    }
+
+    public Optional<Utente> getUserById(int id) {
+        return utenteRepository.findById(id);
+    }
+
+    public Utente addUser(Utente utente) {
+        return utenteRepository.save(utente);
+    }
+
+    public void deleteUser(int id) {
+        utenteRepository.deleteById(id);
+    }
+}
