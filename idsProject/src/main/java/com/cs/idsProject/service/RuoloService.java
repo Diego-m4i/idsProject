fb9e3e1c011340bcfb384425475420b75a3975d1
Diego Marinangeli <marinangelidiego@gmail.com>
2024-06-04 15:37:52 +0200

    RuoloService

---
+++ idsProject/src/main/java/com/cs/idsProject/service/RuoloService.java
@@ -0,0 +1,32 @@
+package com.cs.idsProject.service;
+
+import com.cs.idsProject.entity.Ruolo;
+import com.cs.idsProject.repository.RuoloRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RuoloService {
+
+    @Autowired
+    private RuoloRepository ruoloRepository;
+
+    public List<Ruolo> getAllRuoli() {
+        return ruoloRepository.findAll();
+    }
+
+    public Optional<Ruolo> getRuoloById(int id) {
+        return ruoloRepository.findById(id);
+    }
+
+    public Ruolo addRuolo(Ruolo ruolo) {
+        return ruoloRepository.save(ruolo);
+    }
+
+    public void deleteRuolo(int id) {
+        ruoloRepository.deleteById(id);
+    }
+}
