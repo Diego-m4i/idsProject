fe16c68db9571c0539f299be5fc6e7f6d6642ace
Diego Marinangeli <marinangelidiego@gmail.com>
2024-06-04 11:57:02 +0200

    modifiche poi

--- idsProject/src/main/java/com/cs/idsProject/entity/POI.java
+++ idsProject/src/main/java/com/cs/idsProject/entity/POI.java
@@ -1,4 +1,79 @@
 package com.cs.idsProject.entity;
 
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class POI {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String descrizione;
+    private float longitudine;
+    private float latitudine;
+
+    @OneToMany(mappedBy = "punto")
+    private List<Contenuto> contenuti;
+
+    @ManyToOne
+    @JoinColumn(name = "comune_id")
+    private Comune comune;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public float getLongitudine() {
+        return longitudine;
+    }
+
+    public void setLongitudine(float longitudine) {
+        this.longitudine = longitudine;
+    }
+
+    public float getLatitudine() {
+        return latitudine;
+    }
+
+    public void setLatitudine(float latitudine) {
+        this.latitudine = latitudine;
+    }
+
+    public List<Contenuto> getContenuti() {
+        return contenuti;
+    }
+
+    public void setContenuti(List<Contenuto> contenuti) {
+        this.contenuti = contenuti;
+    }
+
+    public Comune getComune() {
+        return comune;
+    }
+
+    public void setComune(Comune comune) {
+        this.comune = comune;
+    }
 }
