dd4ed67a3437272731b403a55be95b5d4ec31201
Diego Marinangeli <marinangelidiego@gmail.com>
2024-06-04 13:35:31 +0200

    PuntoService.java

---
+++ idsProject/src/main/java/com/cs/idsProject/service/PuntoService.java
@@ -0,0 +1,31 @@
+package com.cs.idsProject.service;
+
+import com.cs.idsProject.entity.POI;
+import com.cs.idsProject.repository.PuntoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PuntoService {
+    @Autowired
+    private PuntoRepository puntoRepository;
+
+    public List<POI> getAllPunti() {
+        return puntoRepository.findAll();
+    }
+
+    public Optional<POI> getPuntoById(int id) {
+        return puntoRepository.findById(id);
+    }
+
+    public POI addPunto(POI punto) {
+        return puntoRepository.save(punto);
+    }
+
+    public void deletePunto(int id) {
+        puntoRepository.deleteById(id);
+    }
+}
