61fd7fd4f7b075ffb5ba2e67e0eaa5d1405143aa
Diego Marinangeli <marinangelidiego@gmail.com>
2024-06-04 11:27:42 +0200

    modifiche Contest

--- idsProject/src/main/java/com/cs/idsProject/entity/Contest.java
+++ idsProject/src/main/java/com/cs/idsProject/entity/Contest.java
@@ -1,4 +1,84 @@
 package com.cs.idsProject.entity;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Contest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String comune;
+    private String titolo;
+    private String descrizione;
+    private boolean pubblico;
+    private int idAnimatore;
+    private String dataScadenza;
+
+    @OneToMany(mappedBy = "contest")
+    private List<Contenuto> contenuti;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getComune() {
+        return comune;
+    }
+
+    public void setComune(String comune) {
+        this.comune = comune;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public boolean isPubblico() {
+        return pubblico;
+    }
+
+    public void setPubblico(boolean pubblico) {
+        this.pubblico = pubblico;
+    }
+
+    public int getIdAnimatore() {
+        return idAnimatore;
+    }
+
+    public void setIdAnimatore(int idAnimatore) {
+        this.idAnimatore = idAnimatore;
+    }
+
+    public String getDataScadenza() {
+        return dataScadenza;
+    }
+
+    public void setDataScadenza(String dataScadenza) {
+        this.dataScadenza = dataScadenza;
+    }
+
+    public List<Contenuto> getContenuti() {
+        return contenuti;
+    }
+
+    public void setContenuti(List<Contenuto> contenuti) {
+        this.contenuti = contenuti;
+    }
 }
