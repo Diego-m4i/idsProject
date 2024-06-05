93d3d977b84740721eec7694c7059da7cdb01a6c
Diego Marinangeli <marinangelidiego@gmail.com>
2024-06-05 10:55:47 +0200

    modifica Comune.java

--- idsProject/src/main/java/com/cs/idsProject/entity/Comune.java
+++ idsProject/src/main/java/com/cs/idsProject/entity/Comune.java
@@ -3,30 +3,38 @@ package com.cs.idsProject.entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Comune {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String nome;
     private String codiceCatasto;
     private String regione;
     private String codistat;
     private float latitudine;
     private float longitudine;
-
-    @OneToMany(mappedBy = "comune")
+    private List<Contest> contest;
     private List<POI> punti;
 
-    @OneToMany(mappedBy = "comune")
-    private List<Contest> contests;
+    public Comune(int id, String nome, String codiceCatasto, String regione, String codistat, float latitudine, float longitudine, List<POI> punti, List<Contest> contest) {
+        this.id = id;
+        this.nome = nome;
+        this.codiceCatasto = codiceCatasto;
+        this.regione = regione;
+        this.codistat = codistat;
+        this.latitudine = latitudine;
+        this.longitudine = longitudine;
 
-    // Getters and setters
+        this.punti = new ArrayList<>(); // Inizializza la lista dei punti di interesse.
+        this.contest = new ArrayList<>(); // Inizializza la lista dei contest.
+
+    }
 
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
@@ -90,11 +98,61 @@ public class Comune {
         this.punti = punti;
     }
 
-    public List<Contest> getContests() {
-        return contests;
+    public void setContest(List<Contest> contest) {
+        this.contest = contest;
     }
 
-    public void setContests(List<Contest> contests) {
-        this.contests = contests;
+
+    public List<POI> getPoints(){
+        return punti; // Restituisce la lista dei punti di interesse associati alla città.
     }
+
+    public POI getPoint(Integer id) {
+        for(POI point: this.punti) {
+            if(point.getId().equals(id)) return point; // Restituisce il punto di interesse con l'ID specificato.
+        }
+        return null; // Se non viene trovato nessun punto di interesse con l'ID specificato, restituisce null.
+    }
+
+    public void addPoint(POI point) {
+        this.punti.add(point); // Aggiunge un punto di interesse alla lista dei punti di interesse associati alla città.
+    }
+
+    public void removePoint(Integer id) {
+        this.punti.removeIf(p -> p.getId().equals(id)); // Rimuove un punto di interesse dalla lista dei punti di interesse associati alla città.
+    }
+
+
+
+    public List<Contest> getContest(){
+        return contest; // Restituisce la lista dei contest associati alla città.
+    }
+
+    public void addContest(Contest contest){
+        this.contest.add(contest); // Aggiunge un contest alla lista dei contest associati alla città.
+    }
+
+    public void removeContest(Integer id) {
+        this.contest.removeIf(c -> c.getId().equals(id)); // Rimuove un contest dalla lista dei contest associati alla città.
+    }
+
+    public Contest getContest(Integer id){
+        for(Contest contest: this.contest) {
+            if(contest.getId().equals(id)) return contest; // Restituisce il contest con l'ID specificato.
+        }
+        return null; // Se non viene trovato nessun contest con l'ID specificato, restituisce null.
+    }
+
+    public void addContent(Contenuto content, Integer pointId){
+        this.punti.get(pointId).addContent(content); // Aggiunge un contenuto a un punto di interesse specifico.
+    }
+
+    public void removeContent(Integer contentId, Integer pointId){
+        this.punti.get(pointId).removeContent(contentId); // Rimuove un contenuto da un punto di interesse specifico.
+    }
+
+    public Contenuto getContent(Integer contentId, Integer pointId){
+        return this.getPoint(pointId).getContenuti(contentId); // Restituisce un contenuto da un punto di interesse specifico.
+    }
+
 }
