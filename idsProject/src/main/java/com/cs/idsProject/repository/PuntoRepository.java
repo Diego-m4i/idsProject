1c07af3a5db6af4c02102aa0d8e53a9a78b9de41
diego <marinangeli.diego@gmail.com>
2024-06-09 19:41:23 +0200

    modifiche PuntoRepository.java

--- idsProject/src/main/java/com/cs/idsProject/repository/PuntoRepository.java
+++ idsProject/src/main/java/com/cs/idsProject/repository/PuntoRepository.java
@@ -1,9 +1,10 @@
 package com.cs.idsProject.repository;
 
-
-
 import com.cs.idsProject.entity.POI;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface PuntoRepository extends JpaRepository<POI, Integer> {
+    Optional<POI> findByNomeAndLatitudineAndLongitudineAndIdComune(String nome, float latitudine, float longitudine, Integer idComune);
+    Optional<POI> findByIdAndIdComune(Integer id, Integer idComune);
 }
