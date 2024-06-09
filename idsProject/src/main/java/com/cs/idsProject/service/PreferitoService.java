d2f3a5611b729b94b186decb62a35d5a5b31616a
diego <marinangeli.diego@gmail.com>
2024-06-09 19:19:22 +0200

    PreferitoService.java.java.java

---
+++ idsProject/src/main/java/com/cs/idsProject/service/PreferitoService.java
@@ -0,0 +1,30 @@
+package com.cs.idsProject.service;
+
+import com.cs.idsProject.entity.Preferito;
+import com.cs.idsProject.entity.Utente;
+import com.cs.idsProject.entity.Contenuto;
+import com.cs.idsProject.repository.PreferitoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PreferitoService {
+
+    @Autowired
+    private PreferitoRepository preferitoRepository;
+
+    public Preferito aggiungiPreferito(Utente utente, Contenuto contenuto) {
+        Preferito preferito = new Preferito(utente, contenuto);
+        return preferitoRepository.save(preferito);
+    }
+
+    public void rimuoviPreferito(Integer id) {
+        preferitoRepository.deleteById(id);
+    }
+
+    public List<Preferito> ottieniPreferitiPerUtente(Utente utente) {
+        return preferitoRepository.findByUtente(utente);
+    }
+}
