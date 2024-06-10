d9c059d9ac51d815f8111717ed61d8683de498fc
diego <marinangeli.diego@gmail.com>
2024-06-10 23:05:02 +0200

    modifica RichiestaAccreditamento

--- idsProject/src/main/java/com/cs/idsProject/entity/RichiestaAccreditamento.java
+++ idsProject/src/main/java/com/cs/idsProject/entity/RichiestaAccreditamento.java
@@ -1,28 +1,18 @@
 package com.cs.idsProject.entity;
 
+import org.apache.catalina.util.Introspection;
+
 import javax.persistence.*;
 
 @Entity
 public class RichiestaAccreditamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private int idRuolo;
-    private int idUtente;
+    private Integer idRuolo;
+    private Integer idUtente;
 
     @Enumerated(EnumType.STRING)
     private StatoRichiesta stato;
-
-    // getters and setters
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public int getIdRuolo() {
         return idRuolo;
