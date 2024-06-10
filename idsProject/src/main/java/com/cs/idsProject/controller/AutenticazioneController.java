3a8fa8643dd7b5d144113502c3c7d952250b482d
diego <marinangeli.diego@gmail.com>
2024-06-10 23:38:42 +0200

    creazione AutenticazioneController

---
+++ idsProject/src/main/java/com/cs/idsProject/controller/AutenticazioneController.java
@@ -0,0 +1,37 @@
+package com.cs.idsProject.controller;
+
+import com.cs.idsProject.entity.Utente;
+import com.cs.idsProject.service.ServizioAutenticazione;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AutenticazioneController {
+
+    @Autowired
+    private ServizioAutenticazione servizioAutenticazione;
+
+    @PostMapping("/login")
+    public ResponseEntity<Utente> login(@RequestParam String username, @RequestParam String password) {
+        Optional<Utente> utente = servizioAutenticazione.autenticazione(username, password);
+        if (utente.isPresent()) {
+            return ResponseEntity.ok(utente.get());
+        } else {
+            return ResponseEntity.status(401).build(); // Unauthorized
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Utente> register(@RequestBody Utente utente) {
+        try {
+            Utente nuovoUtente = servizioAutenticazione.registrazione(utente);
+            return ResponseEntity.ok(nuovoUtente);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(null); // Bad Request
+        }
+    }
+}
